4150b73a815c8f0493efdc02c451c09e3b09f212
trswork <87744214+trswork@users.noreply.github.com>
2021-12-04 20:28:44 -0500

    update

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -12,19 +12,17 @@ const {
 
 // Set up GET all and POST at /api/thought
 router.route('/')
-  .get(getAllThought);
-
-router.route('/:userId')
+  .get(getAllThought)
   .post(addThought);
 
 // Set up GET one, PUT, and DELETE at /api/thought/:id
-router.route('/:thoughtId')
+router.route('/:id')
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
 
 // Create a PUT and DELETE at /api/thoughts/:userId/:thoughtId
-router.route('/:userId/:thoughtId')
+router.route('/:thoughtId/reactions')
   .put(addReaction)
   .delete(deleteReaction)
 
